25d83b0ab926dc4a8dc61abf5e4ed4891e502d12
Spagero 763 <afolabiayomide870@gmail.com>
2026-01-28 18:21:09 +0100

    improve deploy script with console logging

--- script/DeployPaymentVault.s.sol
+++ script/DeployPaymentVault.s.sol
@@ -3,13 +3,22 @@
 pragma solidity ^0.8.19;
 
 import {PaymentVault} from "../src/PaymentVault.sol";
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 
 contract DeployPaymentVault is Script {
-    function run() external returns (PaymentVault){
-        vm.startBroadcast();
-        PaymentVault paymentvault = new PaymentVault();
+    function run() external returns (PaymentVault) {
+        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+        
+        vm.startBroadcast(deployerKey);
+        
+        PaymentVault vault = new PaymentVault();
+        
+        console.log("PaymentVault deployed at:", address(vault));
+        console.log("Owner:", vault.owner());
+        console.log("Min deposit:", vault.MIN_DEPOSIT());
+        
         vm.stopBroadcast();
-        return paymentvault;
+        
+        return vault;
     }
 }
